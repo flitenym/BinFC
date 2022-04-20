63a8e3a5eedf3629b7b9d1a2d99e33821dd83760
Долматкин Андрей <flitenym@gmail.com>
2022-04-20 12:52:06 +0500

    Вызов всех модулей

--- Server/BinFC/FatCamel.Host/Startup.cs
+++ Server/BinFC/FatCamel.Host/Startup.cs
@@ -1,16 +1,28 @@
+using FatCamel.Host.Interfaces;
+using FatCamel.Host.StaticClasses;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Localization;
 using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace FatCamel.Host
 {
     public class Startup
     {
-        public Startup()
+        private IModule[] _modules;
+        private IConfiguration _configuration { get; }
+
+        public Startup(IConfiguration configuration)
         {
+            _configuration = configuration;
+            _modules = StartupManager.Graph?.Select(m => m.CreateInstance(_configuration)).Where(m => m != null).Cast<IModule>().ToArray() ?? new IModule[0];
         }
 
         public IConfiguration Configuration { get; }
@@ -22,6 +34,14 @@ namespace FatCamel.Host
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "FatCamel.Host", Version = "v1" });
             });
+
+            services.TryAddTransient(typeof(IStringLocalizer<>), typeof(StringLocalizer<>));
+
+            foreach (var m in _modules)
+            {
+                m.ConfigureServices(services);
+            }
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,6 +53,17 @@ namespace FatCamel.Host
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "FatCamel.Host v1"));
             }
+
+            app.UseStaticFiles();
+
+            foreach (var m in _modules)
+                m.Configure(app, env);
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
 
             app.UseHttpsRedirection();
 
