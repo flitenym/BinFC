b008f63351da93a62b8afbd5a07910f04c840d98
Долматкин Андрей <flitenym@gmail.com>
2022-05-06 04:23:42 +0500

    front

---
+++ Server/BinFC/Storage.Module/Classes/SettingsInfo.cs
@@ -0,0 +1,33 @@
+﻿namespace Storage.Module.Classes
+{
+    public class SettingsInfo
+    {
+        public string ApiKey { get; set; }
+        public string ApiSecret { get; set; }
+        public string Emails { get; set; }
+        public string EmailLogin { get; set; }
+        public string EmailPassword { get; set; }
+        public string CronExpression { get; set; }
+        public string SellCurrency { get; set; }
+
+        public (bool IsValid, string ValidError) IsValid()
+        {
+            if (string.IsNullOrEmpty(ApiKey))
+            {
+                return (false, "ApiKey не задан");
+            }
+
+            if (string.IsNullOrEmpty(ApiSecret))
+            {
+                return (false, "ApiSecret не задан");
+            }
+
+            if (string.IsNullOrEmpty(SellCurrency))
+            {
+                return (false, "Валюта для продажи не задана");
+            }
+
+            return (true, null);
+        }
+    }
+}
